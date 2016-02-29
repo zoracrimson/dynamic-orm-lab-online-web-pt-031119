e7acc77c5ea448347d14b24916abc63a03fb6edf
joshuabamboo <joshuaowens011@gmail.com>
2016-02-29 18:03:20 -0500

    add solution to solution branch

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,10 +3,11 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   def self.table_name
-    "#{self.to_s.downcase}s"
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
 
