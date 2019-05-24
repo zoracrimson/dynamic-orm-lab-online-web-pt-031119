0f5d60442be12282c560e9ca26ffad92a8d55b85
Kat Maldon <katharinamaldon@gmail.com>
2019-05-24 16:52:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 
+class Student < InteractiveRecord
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
 end
